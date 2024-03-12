68614b2f675259af2a70f45e771d9d74f2f7e95b
igalonso <igalonso@gmail.com>
2024-03-12 11:09:22 +0100

    big app update

--- bigapp/🏠_Home.py
+++ bigapp/🏠_Home.py
@@ -1,28 +1,30 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="oneweekoneusecase",
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Hello LinkedIn Fam! 👋")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    This is a space where you can test all of the demos I've created!\n
+    """)
+st.image("assets/intro.png")
+st.markdown(
+    """
+    In the left panel you will see all the demos available.\n
+    To test them out, go to this link and select the resources to be used on each demo. [go link](http://go/oneweekoneusecase)\n
+    Find the best resources with the name of the demo on each of them. \n
+    Of course, feel free to test them with your own resources as well but keep in mind that the results may vary (or not!).\n
+    It is only for demo purposes, so retry if it fails, don't shot the messenger!😅\n
+    And, of course, if you have any questions, feel free to reach out to me!\n
+
+    To follow me on LinkedIn, click [here](https://www.linkedin.com/in/igngar) and get all the new demos every week \n
+    [igngar@](https://moma.corp.google.com/person/igngar) - Nacho Garcia
+
 """
 )
